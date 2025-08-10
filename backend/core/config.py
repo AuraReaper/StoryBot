3301755f8feb07ccf1714bb2894b826eb6befce1
AuraReaper <yashkr104@gmail.com>
2025-08-10 16:46:30 +0530

    added db connection

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,8 @@
+from logging import DEBUG
 from typing import List
 from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -8,12 +10,22 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
         case_sensitive=True
     )
-    
-    DATABASE_URL: str
+
+    DATABASE_URL: str = None
     API_PREFIX: str = "/api"
     DEBUG: bool = True
     ALLOWED_ORIGINS: str = ""
     GEMINI_API_KEY: str
+
+    def __init__(self, **values):
+        super().__init__(**values)
+        if not self.DEBUG:
+            db_user = os.getenv("DB_USER")
+            db_password = os.getenv("DB_PASSWORD")
+            db_host = os.getenv("DB_HOST")
+            db_port = os.getenv("DB_PORT")
+            db_name = os.getenv("DB_NAME")
+            self.DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
     @field_validator("ALLOWED_ORIGINS")
     def parse_allowed_origins(cls, value) -> List[str]:
